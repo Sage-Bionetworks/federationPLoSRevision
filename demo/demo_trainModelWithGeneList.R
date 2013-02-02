6b7f10861af45fed5ff1da843c57f1154b79d84a
AAMargolin <margolin@sagebase.org>
2013-02-02 23:20:15 +0000

    gene list demo

--- demo/demo_trainModelWithGeneList.R
+++ demo/demo_trainModelWithGeneList.R
@@ -1,6 +1,31 @@
-require(synapseClient)
-        
-demo_trainModelWithGeneList <- function(synapseGeneListId){
-  geneListEntity <- loadEntity("syn1643406")
+library(devtools)
+
+myGithubUsername <- "AAMargolin"
+myGithubPassword <- "Set password here"
+install_github("federationPLoSRevision", username="Sage-Bionetworks", auth_user=myGithubUsername, password=myGithubPassword)
+
+require(federationPLoSRevision)
+
+demo_trainModelWithGeneList <- function(synapseGeneListId = "syn1652976"){
+  geneListEntity <- loadEntity(synapseGeneListId)
+  geneList <- read.table(paste(geneListEntity$cacheDir, geneListEntity$files[[1]], sep="/"), stringsAsFactors=FALSE)
+  geneList <- geneList[,1]
+  
+  metabricTrainingData <- loadMetabricMicmaTrainingData()
+  
+  exprData <- exprs(metabricTrainingData$exprData)
+  exprData_filtered <- exprData[which(rownames(exprData) %in% geneList), ]
+  
+  copyData <- exprs(metabricTrainingData$copyData)
+  copyData_filtered <- copyData[which(rownames(copyData) %in% geneList), ]
+  
+  coxTestModel <-CoxphModel$new()
+  
+  coxTestModel$customTrain(exprData_filtered, copyData_filtered,
+                           metabricTrainingData$clinicalFeaturesData, metabricTrainingData$clinicalSurvData)
+  
+  modelClassFile <- "/home/ubuntu/SageGit/Sage-Bionetworks/federationPLoSRevision/R/CoxphModel.R"
+  submitCompetitionModel_micmaTrained(modelName = "CoxTestModel_mammapringGenes", trainedModel=coxTestModel, rFiles=modelClassFile,
+                                      algorithm="Cox", geneList=geneListEntity$properties$name)
   
 }

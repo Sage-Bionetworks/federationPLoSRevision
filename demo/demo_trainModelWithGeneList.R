a4e7ce0bc10506bd1dee61c51b89bd494b3d376b
AAMargolin <margolin@sagebase.org>
2013-02-02 23:51:48 +0000

    updated gene list demo.

--- demo/demo_trainModelWithGeneList.R
+++ demo/demo_trainModelWithGeneList.R
@@ -11,21 +11,21 @@ demo_trainModelWithGeneList <- function(synapseGeneListId = "syn1652976"){
   geneList <- read.table(paste(geneListEntity$cacheDir, geneListEntity$files[[1]], sep="/"), stringsAsFactors=FALSE)
   geneList <- geneList[,1]
   
-  metabricTrainingData <- loadMetabricMicmaTrainingData()
+  metabricMicmaTrainingData <- loadMetabricMicmaTrainingData()
   
-  exprData <- exprs(metabricTrainingData$exprData)
+  exprData <- exprs(metabricMicmaTrainingData$exprData)
   exprData_filtered <- exprData[which(rownames(exprData) %in% geneList), ]
   
-  copyData <- exprs(metabricTrainingData$copyData)
+  copyData <- exprs(metabricMicmaTrainingData$copyData)
   copyData_filtered <- copyData[which(rownames(copyData) %in% geneList), ]
   
   coxTestModel <-CoxphModel$new()
   
   coxTestModel$customTrain(exprData_filtered, copyData_filtered,
-                           metabricTrainingData$clinicalFeaturesData, metabricTrainingData$clinicalSurvData)
+                           metabricMicmaTrainingData$clinicalFeaturesData, metabricMicmaTrainingData$clinicalSurvData)
   
   modelClassFile <- "/home/ubuntu/SageGit/Sage-Bionetworks/federationPLoSRevision/R/CoxphModel.R"
-  submitCompetitionModel_micmaTrained(modelName = "CoxTestModel_mammapringGenes", trainedModel=coxTestModel, rFiles=modelClassFile,
+  submitCompetitionModel_micmaTrained(modelName = "CoxTestModel_mammaprintGenes", trainedModel=coxTestModel, rFiles=modelClassFile,
                                       algorithm="Cox", geneList=geneListEntity$properties$name)
   
 }
